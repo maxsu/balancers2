1931a2ff392e3f020218985e8f420a87ac0cb02a
Max Suica <max.suica@gmail.com>
2022-02-22 17:05:46 -0800

    Add utility functions for test cases

--- test_cases.cpp
+++ test_cases.cpp
@@ -5,80 +5,56 @@ struct TestNet {
   Row ratios;
 };
 
+// Test case utility functions
+
+// Generate an empty network
+Network emptyNetwork(int size) {
+  Network nodes;
+  for (int i = 0; i < size; ++i) {
+    Node* node = new Node;
+    nodes.push_back(node);
+  }
+  return nodes;
+}
+
+// Link two nodes in a network
+void link(Network& nodes, int source, int target) {
+  Node* target_node = nodes[target];
+  Node* source_node = nodes[source];
+  source_node->outputs.push_back(target_node);
+  target_node->inputs.push_back(source_node);
+}
+
+// Test cases
+
 // Set up a 17 node test network
 TestNet testnet_A() {
-  Node* s0 = new Node;
-  Node* s1 = new Node;
-  Node* s2 = new Node;
-  Node* s3 = new Node;
-  Node* s4 = new Node;
-  Node* s5 = new Node;
-  Node* s6 = new Node;
-  Node* s7 = new Node;
-  Node* s8 = new Node;
-  Node* s9 = new Node;
-  Node* s10 = new Node;
-  Node* s11 = new Node;
-  Node* s12 = new Node;
-  Node* s13 = new Node;
-  Node* s14 = new Node;
-  Node* s15 = new Node;
-  Node* s16 = new Node;
+  Network nodes = emptyNetwork(17);
 
-  // inputs
-  s1->inputs.push_back(s0);
-  s1->inputs.push_back(s2);
-  s2->inputs.push_back(s1);
-  s2->inputs.push_back(s5);
-  s3->inputs.push_back(s1);
-  s3->inputs.push_back(s9);
-  s4->inputs.push_back(s2);
-  s5->inputs.push_back(s8);
-  s5->inputs.push_back(s11);
-  s6->inputs.push_back(s4);
-  s7->inputs.push_back(s3);
-  s8->inputs.push_back(s3);
-  s8->inputs.push_back(s6);
-  s9->inputs.push_back(s4);
-  s9->inputs.push_back(s7);
-  s10->inputs.push_back(s7);
-  s11->inputs.push_back(s10);
-  s11->inputs.push_back(s8);
-  s12->inputs.push_back(s9);
-  s12->inputs.push_back(s10);
-  s13->inputs.push_back(s12);
-  s14->inputs.push_back(s11);
-  s15->inputs.push_back(s6);
-  s16->inputs.push_back(s5);
-
-  // outputs
-  s0->outputs.push_back(s1);
-  s2->outputs.push_back(s1);
-  s1->outputs.push_back(s2);
-  s5->outputs.push_back(s2);
-  s1->outputs.push_back(s3);
-  s9->outputs.push_back(s3);
-  s2->outputs.push_back(s4);
-  s8->outputs.push_back(s5);
-  s11->outputs.push_back(s5);
-  s4->outputs.push_back(s6);
-  s3->outputs.push_back(s7);
-  s3->outputs.push_back(s8);
-  s6->outputs.push_back(s8);
-  s4->outputs.push_back(s9);
-  s7->outputs.push_back(s9);
-  s7->outputs.push_back(s10);
-  s10->outputs.push_back(s11);
-  s8->outputs.push_back(s11);
-  s9->outputs.push_back(s12);
-  s10->outputs.push_back(s12);
-  s12->outputs.push_back(s13);
-  s11->outputs.push_back(s14);
-  s6->outputs.push_back(s15);
-  s5->outputs.push_back(s16);
-
-  Network network = {s0, s1,  s2,  s3,  s4,  s5,  s6,  s7, s8,
-                     s9, s10, s11, s12, s13, s14, s15, s16};
+  link(nodes, 0, 1);
+  link(nodes, 2, 1);
+  link(nodes, 1, 2);
+  link(nodes, 5, 2);
+  link(nodes, 1, 3);
+  link(nodes, 9, 3);
+  link(nodes, 2, 4);
+  link(nodes, 8, 5);
+  link(nodes, 11, 5);
+  link(nodes, 4, 6);
+  link(nodes, 3, 7);
+  link(nodes, 3, 8);
+  link(nodes, 6, 8);
+  link(nodes, 4, 9);
+  link(nodes, 7, 9);
+  link(nodes, 7, 10);
+  link(nodes, 10, 11);
+  link(nodes, 8, 11);
+  link(nodes, 9, 12);
+  link(nodes, 10, 12);
+  link(nodes, 12, 13);
+  link(nodes, 11, 14);
+  link(nodes, 6, 15);
+  link(nodes, 5, 16);
 
   Row ratios = {1,
                 0.75641024112701416,
@@ -98,44 +74,24 @@ TestNet testnet_A() {
                 0.12820513546466827,
                 0.26923078298568726};
 
-  TestNet result = {network, ratios};
-  return result;
+  TestNet A = {nodes, ratios};
+  return A;
 }
 
 // Set up a 8 node test network
 TestNet testnet_B() {
-  Node* s1 = new Node;
-  Node* s2 = new Node;
-  Node* s3 = new Node;
-  Node* s4 = new Node;
-  Node* s5 = new Node;
-  Node* s6 = new Node;
-  Node* s7 = new Node;
-  Node* s8 = new Node;
+  Network nodes = emptyNetwork(8);
 
-  // inputs
-  s3->inputs.push_back(s1);
-  s3->inputs.push_back(s2);
-  s4->inputs.push_back(s5);
-  s5->inputs.push_back(s4);
-  s5->inputs.push_back(s3);
-  s6->inputs.push_back(s4);
-  s6->inputs.push_back(s3);
-  s7->inputs.push_back(s5);
-  s8->inputs.push_back(s6);
+  link(nodes, 0, 2);
+  link(nodes, 1, 2);
+  link(nodes, 2, 4);
+  link(nodes, 2, 5);
+  link(nodes, 3, 4);
+  link(nodes, 3, 5);
+  link(nodes, 4, 3);
+  link(nodes, 4, 6);
+  link(nodes, 5, 7);
 
-  // outputs
-  s1->outputs.push_back(s3);
-  s2->outputs.push_back(s3);
-  s3->outputs.push_back(s5);
-  s3->outputs.push_back(s6);
-  s4->outputs.push_back(s5);
-  s4->outputs.push_back(s6);
-  s5->outputs.push_back(s4);
-  s5->outputs.push_back(s7);
-  s6->outputs.push_back(s8);
-
-  Network network = {s1, s2, s3, s4, s5, s6, s7, s8};
   Row ratios = {1,
                 0,
                 0.5,
@@ -145,6 +101,6 @@ TestNet testnet_B() {
                 0.3333333432674408,
                 0.66666668653488159};
 
-  TestNet result = {network, ratios};
-  return result;
+  TestNet B = {nodes, ratios};
+  return B;
 }
