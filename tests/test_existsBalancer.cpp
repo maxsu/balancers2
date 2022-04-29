c5a177366334251c5cc9f6a535797edc26c70ce3
Max Suica <max.suica@gmail.com>
2022-04-28 19:16:59 -0700

    Build Example for profiling

--- tests/test_existsBalancer.cpp
+++ tests/test_existsBalancer.cpp
@@ -5,19 +5,8 @@
 #include "../lib/exists_balancer.hpp"
 #include "catch2/catch.hpp"
 
-unsigned int Factorial2(unsigned int number) {
-  return number <= 1 ? number : Factorial2(number - 1) * number;
-}
-
 TEST_CASE("existsBalancer") {
   SECTION("Balancer 1-1-1 exists") { REQUIRE(existsBalancer(1, 1, 1)); }
   SECTION("Balancer 3-3-2 doesn't exist") { REQUIRE(!existsBalancer(3, 3, 2)); }
   SECTION("Balancer 4-4-4 exists") { REQUIRE(existsBalancer(4, 4, 4)); }
 }
-
-// vector<vector<double>> my_network = {{0.1, 0.2, 0.3, 0.4}, {0.5, 0.6, 0.7,
-// 0.8}};
-
-// vector<vector<double>> new_network = addSplitter(my_network, {0}, {1, -1});
-
-// bool balancerExists = existsBalancer(5, 5, 6);
